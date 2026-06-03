da1510e6f1b2fbea2eab027bb975ca64452a3df1
sahil7249 <pathansahil7249@gmail.com>
2026-06-03 21:58:59 +0530

    add : authMiddleware

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import ApiError from "../utils/apiError.js";
+import jwt from "jsonwebtoken";
+
+export const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(new ApiError(401, "Unauthorized"));
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    next(new ApiError(401, "Invalid token"));
+  }
+};
