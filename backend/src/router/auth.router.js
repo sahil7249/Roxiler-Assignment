31c06b43ea135b9c95db6315cf268f382a5c32ce
sahil7249 <pathansahil7249@gmail.com>
2026-06-03 22:01:30 +0530

    add : changePassword route

--- backend/src/router/auth.router.js
+++ backend/src/router/auth.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controller/auth.controller.js";
+import { changePassword, login, register } from "../controller/auth.controller.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const authRouter = Router();
 
 authRouter.post('/register',register)
 authRouter.post('/login',login)
+authRouter.put('/change-password',authenticate,changePassword)
 
 export default authRouter;
